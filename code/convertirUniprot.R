f43427ba55a0400cef7a04437cb256dcb608ccfa
InmaJimenez26 <inmajimenezpalomino@uma.es>
2022-01-24 11:55:28 +0100

    Terminado convertirUniprot.R

--- code/convertirUniprot.R
+++ code/convertirUniprot.R
@@ -13,17 +13,18 @@ proteinas_entrada <- args[1]
 fichero_salida <- args[2]
 
 data <- read_table(proteinas_entrada, col_names = FALSE)
+colnames(data)[1] <- "GENENAME"
 
 convertir <- function(gene_uniprot) {
   require(clusterProfiler)
   require(org.Hs.eg.db)
-  gnm <-tryCatch(bitr(gene_uniprot, fromType="GENENAME", toType="UNIPROT", OrgDb="org.Hs.eg.db"),error=function(e){NA})
+  gnm <-tryCatch(bitr(gene_uniprot, fromType="ALIAS", toType="UNIPROT", OrgDb="org.Hs.eg.db"),error=function(e){NA})
   gnm <- tryCatch(if(!is.na(gnm)){gnm <- gnm$UNIPROT},error=function(e){NA})
   
   return(as.character(gnm[1]))
 }
 
-data$UNIPROT <- sapply(data$X1, convertir)
+data$UNIPROT <- sapply(data$GENENAME, convertir)
 
 write.table(data$UNIPROT, fichero_salida, quote = FALSE, row.names = FALSE, col.names = FALSE)
 
