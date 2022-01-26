b385b09ad6c2217ee06265d8a58610002cd9bdaa
InmaJimenez26 <inmajimenezpalomino@uma.es>
2022-01-26 16:53:42 +0100

    Eliminado paso de conversiona Chembl

--- code/launch.sh
+++ code/launch.sh
@@ -11,16 +11,12 @@ echo "2. Transformamos el ID de las proteínas a Uniprot, para usarlos en Chembl
 Rscript code/convertirUniprot.R results/human_proteins.txt results/human_proteins_uniprot.txt
 echo "Archivo results/human_proteins_uniprot.txt creado"
 
-echo "3. Transformamos el UniProt ID de las proteínas a Chembl ID, buscándolos en Chembl DB"
-Rscript code/convertirChembl.py results/human_proteins_uniprot.txt results/human_proteins_chembl.txt
-echo "Archivo results/human_proteins_chembl.txt creado"
-
-echo "4. Buscamos en Chembl los fármacos asociados a estas proteínas"
-python3 code/consultarChembl.py results/human_proteins_chembl.txt 4 results/farmacos_aprobados.csv
-python3 code/consultarChembl.py results/human_proteins_chembl.txt 3 results/farmacos_experimentales.csv
+echo "3. Buscamos en Chembl los fármacos asociados a estas proteínas"
+python3 code/consultarChembl.py results/human_proteins_uniprot.txt 4 results/farmacos_aprobados.csv
+python3 code/consultarChembl.py results/human_proteins_uniprot.txt 3 results/farmacos_experimentales.csv
 echo "Archivos results/farmacos_aprobados.csv y results/farmacos_experimentales.csv creados"
 
-echo "5. Análisis de los resultados"
+echo "4. Análisis de los resultados"
 Rscript code/analisisResultados.R results/farmacos_aprobados.csv aprobados
 Rscript code/analisisResultados.R results/farmacos_experimentales.csv experimentales
 echo "Imágenes y gráficas creadas"
