bf455960e04dcdf6e4be38265211830fa3a07d67
talal314 <talal.awija@gmail.com>
2022-01-27 11:45:21 +0100

    Add args to read the file

--- code/consultarChembl.py
+++ code/consultarChembl.py
@@ -3,9 +3,14 @@ import csv
 import sys
 import os
 
-fileExist = os.path.exists('Farmacos_data.csv')
+if(len(sys.argv)!=3):
+    print("Error, use: python3 <python file> <human_proteins_uniprot.txt> <max_phase> <result.csv>")
+
+print(sys.argv[3])
+
+fileExist = os.path.exists(sys.argv[3])
 # Get genes from file.txt
-with open("results/human_proteins_uniprot.txt") as f:
+with open(sys.argv[1]) as f:
     genes = f.read().splitlines()
 
 #print(genes)
@@ -22,7 +27,7 @@ cur = mydb.cursor()
 for gen in genes:
     print(gen)
 
-    sql = "SELECT DISTINCT compound_name, max_phase, ACTION_T.action_type, M.pref_name,CS.accession FROM compound_records C, assays A, target_dictionary T, activities ACT, molecule_dictionary M, drug_mechanism D, action_type ACTION_T,component_sequences CS,target_components TC WHERE C.record_id = ACT.record_id AND ACT.assay_id = A.assay_id AND A.tid = T.tid AND pchembl_value >=6 AND M.molregno = C.molregno AND D.molregno = M.molregno AND D.action_type = ACTION_T.action_type AND max_phase = 4 AND CS.component_id = TC.component_id AND TC.tid = T.tid AND CS.accession=" +"'" +gen+ "'"
+    sql = "SELECT DISTINCT compound_name, max_phase, ACTION_T.action_type, M.pref_name,CS.accession FROM compound_records C, assays A, target_dictionary T, activities ACT, molecule_dictionary M, drug_mechanism D, action_type ACTION_T,component_sequences CS,target_components TC WHERE C.record_id = ACT.record_id AND ACT.assay_id = A.assay_id AND A.tid = T.tid AND pchembl_value >=6 AND M.molregno = C.molregno AND D.molregno = M.molregno AND D.action_type = ACTION_T.action_type AND max_phase = "+ sys.argv[2]+" AND CS.component_id = TC.component_id AND TC.tid = T.tid AND CS.accession=" +"'" +gen+ "'"
     cur.execute(sql)
 
     myresult = cur.fetchall()
@@ -39,8 +44,8 @@ for gen in genes:
 
         for row in myresult:
             result.append(row)
-            fileExist = os.path.exists('results/farmacos_aprobados.csv')
-            with open("results/farmacos_aprobados.csv", 'a', newline='') as csvfile:
+            fileExist = os.path.exists(sys.argv[3])
+            with open(sys.argv[3], 'a', newline='') as csvfile:
                 csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 if fileExist:
                     for row in range(1,len(result)):
